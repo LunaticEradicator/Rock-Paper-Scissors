0ee48edf152a06d94c54ac5663a6e58d41f6b5a8
YeduKrishnan A <kuttuzz17@gmail.com>
2022-08-10 11:34:31 +0530

    Finalize Game Code

--- README.md
+++ README.md
@@ -8,5 +8,8 @@ A, Game Rule
 B, Game Stages
     This game is created using different javascript learning stage
 
-   I, We wrote the basic game code , which will display the game on the console with player and computer score [No EventListener Added ]
-
+    I, We wrote the basic game code , which will display the game on the console with player and computer score [No EventListener Added ]
+    II, Using Dom Manipulation and event listener we created a game with some basic UI with the help of CSS
+    III, When the user press the key, the addEventListener listens for it and display the current level details
+    IV,  After 5 round the we can see who wins the game
+    V,  For restarting we add another addEventListener on the restartButton and it reset the value of score,level to 0 and dom element value to = ""
--- script.js
+++ script.js
@@ -1,75 +1,180 @@
-// set hardcoded computer score
 
+// Set hardcoded value
 let computerInput = ["rock", "paper", "scissors"];
+
+// Score
 let playerScore = 0;
 let computerScore = 0;
+let roundPlayed = 0;
+
+// Dom Manipulation //
+const btn = document.querySelectorAll(".btn")
+const score = document.querySelector(".score")
+const startGame = document.querySelector(".startGame");
+const resetGame = document.querySelector(".resetGame");
+
+
+let roundNumber = document.querySelector(".roundDetails");
+let playerRoundScore = document.createElement("div");
+let computerRoundScore = document.createElement("div");
+let roundStatus = document.createElement("div");
+let eachRoundEndScore = document.createElement("div");
+
+
+let endScore = document.createElement("div");
+let playerWin = document.createElement("div");
+let gameDraw = document.createElement("div");
+let playerLose = document.createElement("div");
+
+let restartButton = document.createElement("button");
 
 // random computerGuess generator
 function computerGuess() {
     let computerGuess = computerInput[Math.floor(Math.random() * computerInput.length)];
-    return (computerGuess);
+    return computerGuess;
 }
 
 // function to play a single round 
-function playSingleRound(playerDraw, computerDraw) {
-    if ((playerDraw === "rock" && computerDraw === "rock") || (playerDraw === "paper" && computerDraw === "paper") || (playerDraw === "scissors" && computerDraw === "scissors")) {
+function gameLogic(playerDraw, computerDraw) {
+
+    if ((playerDraw === "rock" && computerDraw === "rock")
+        || (playerDraw === "paper" && computerDraw === "paper")
+        || (playerDraw === "scissors" && computerDraw === "scissors")) {
         return ("Round Draw");
     }
-    else if (playerDraw === "rock" && computerDraw === "paper") {
+    else if (playerDraw === "rock" && computerDraw === "paper"
+        || playerDraw === "paper" && computerDraw === "scissors"
+        || playerDraw === "scissors" && computerDraw === "rock") {
         computerScore++;
-        return ("Computer won the round");
+        return ("Computer Wins");
     }
-    else if (playerDraw === "rock" && computerDraw === "scissors") {
+    else if (playerDraw === "rock" && computerDraw === "scissors"
+        || playerDraw === "paper" && computerDraw === "rock"
+        || playerDraw === "scissors" && computerDraw === "paper") {
         playerScore++;
-        return ("Player won the round");
-    }
-    else if (playerDraw === "paper" && computerDraw === "rock") {
-        playerScore++;
-        return ("Player won the round");
-    }
-    else if (playerDraw === "paper" && computerDraw === "scissors") {
-        computerScore++;
-
-        return ("Computer Won the round");
-    }
-    else if (playerDraw === "scissors" && computerDraw === "rock") {
-        computerScore++;
-        return ("Computer won the round");
-    }
-    else if (playerDraw === "scissors" && computerDraw === "paper") {
-        playerScore++;
-        return ("Player won the round");
+        return ("Player Wins");
     }
     else {
         return ("Please enter a correct value from the option given : Rock | Paper | Scissors");
     }
 }
 
+// When user press a button call the function of playGame
+
+btn.forEach(btnEach => {
+
+    btnEach.addEventListener("click", (e) => {
+        if (roundPlayed === 5) {
+            btn.disabled = true;
+        }
+        else {
+            userInput = btnEach.textContent.toLowerCase(); // gives the name of userInput//a variable that has not been declared, it will automatically become a GLOBAL variable.
+            playGame();
+            restartGame();
+        }
+    })
+})
+
 
 function playGame() {
-    // function to loop the game 5 times
-    for (let i = 0; i < 5; i++) {
-        userInput = prompt("Select an option : Rock | Paper | Scissors").toLowerCase();
-        playerDraw = userInput;
-        computerDraw = computerGuess();
 
-        console.log(`Player has drawn ${playerDraw}`);
-        console.log(`Computer has drawn ${computerDraw}`);
-        console.log(playSingleRound(playerDraw, computerDraw));
+    playerDraw = userInput;
+    computerDraw = computerGuess();
+    roundPlayed++;
+    eachRoundScore();
+
+    if (roundPlayed === 5) {
+        finalScore();
     }
-    // return the result and declare the winner.
-    console.log(`Player has scored ${playerScore} and Computer has scored ${computerScore}`)
+
+    console.log(`Round    Played ${roundPlayed}`);
+    console.log(`Player   Score ${playerScore}`);
+    console.log(`Computer Score ${computerScore}`);
+    console.log(`----------------------------------`);
+}
+function capitalLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function eachRoundScore() {
+
+    roundNumber.textContent = `Round ${roundPlayed} of 5`;
+    startGame.append(roundNumber);
+
+    playerRoundScore.classList.add("playerRoundScore");
+    playerRoundScore.textContent = `Player Selected: ${capitalLetter(playerDraw)}`;
+    startGame.append(playerRoundScore);
+
+    computerRoundScore.classList.add("computerRoundScore");
+    computerRoundScore.textContent = `Computer Selected: ${capitalLetter(computerDraw)}`;
+    startGame.append(computerRoundScore);
+
+    roundStatus.classList.add("eachRoundFinalScore");
+    roundStatus.textContent = `Status Of The Round: ${gameLogic(playerDraw, computerDraw)}`;
+    startGame.append(roundStatus);
+
+    eachRoundEndScore.classList.add("eachRoundEndScore");
+    eachRoundEndScore.textContent = `Player Score: ${playerScore} and Computer Score: ${computerScore}`;
+    startGame.append(eachRoundEndScore);
+}
+
+function finalScore() {
+
+    eachRoundEndScore.classList.add("eachRoundEndScore");
+    eachRoundEndScore.textContent = `Player Score: ${playerScore} and Computer Score: ${computerScore}`;
+    resetGame.append(eachRoundEndScore);
+
 
     if (playerScore > computerScore) {
-        console.log(`Player has won the game `)
+
+        playerWin.classList.add("playerWin");
+        playerWin.textContent = `Congratulations! The Player has Won the game`;
+        resetGame.append(playerWin);
     }
     else if (playerScore === computerScore) {
-        console.log(` The Game is Draw `)
+
+        gameDraw.classList.add("gameDraw");
+        gameDraw.textContent = `Oh Snap! The game is on a Draw`;
+        resetGame.append(gameDraw);
     }
     else {
-        console.log(` Computer has won the game`)
+        playerLose.classList.add("playerLose");
+        playerLose.textContent = `Seems like you are not even better than a BOT`;
+        resetGame.append(playerLose);
     }
 }
 
-// call the function then only the player can enter the prompt
-playGame();
+
+
+function restartGame() {
+    if (roundPlayed === 5) {
+        restartButton.textContent = `Restart`;
+        resetGame.append(restartButton);
+        startGame.hidden = true;
+        resetGame.hidden = false;
+
+        restartButton.addEventListener("click", (e) => {
+            console.log("Pressed Restart");
+
+            startGame.hidden = false;
+            resetGame.hidden = true;
+
+
+            playerScore = 0;
+            computerScore = 0;
+            roundPlayed = 0;
+
+            playerRoundScore.textContent = "";
+            computerRoundScore.textContent = "";
+            eachRoundEndScore.textContent = "";
+            roundStatus.textContent = "";
+            playerWin.textContent = "";
+            gameDraw.textContent = "";
+            playerLose.textContent = "";
+            roundNumber.textContent = "Round Details";
+        })
+    }
+}
+
+
+
